7eec682dc3dea1c3f01322952b65e17035c280f7
michel novellino <michelnovellino.programador@gmail.com>
2020-01-11 15:40:36 -0400

    allow cors

--- app.js
+++ app.js
@@ -38,6 +38,14 @@ app.use(function(req, res, next) {
 app.disable('x-powered-by');
 
 // error handler
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
