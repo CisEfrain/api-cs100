7280656ee30dc8b3560787fa2bcdf42a959c2479
michel novellino <michelnovellino.programador@gmail.com>
2020-05-25 14:47:00 -0400

    mailer

--- controladores/mailer.js
+++ controladores/mailer.js
@@ -4,11 +4,23 @@ var c = require("../config/config");
 // async..await is not allowed in global scope, must use a wrapper
 exports.sendEmail = function (req, res) {
     //res.json(c.mailu.u)
-
+    let subject = "Nueva Solicitud de cambio de contraseña"
+    let html = "";// html body
     // Generate test SMTP service account from ethereal.email
     // Only needed if you don't have a real mail account for testing
-    let receiver = req.body
-    console.log(receiver)
+    let user_type = '';
+    let { email, username, type } = req.body
+    if (type == "watcher") {
+        user_type = "Vigilante"
+    } else {
+        user_type = "Residente"
+    }
+    html = `
+        <h1> Se ha solicitado un cambio de contraseña para el siguiente usuario </h1>
+        <h3> tipo de usuario: </h3> ${user_type || "No recibido"} <br/>
+        <h3> identificador: </h3> ${email || username} <br/>
+        `
+    console.log(email + " " + username)
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",// true for 465, false for other ports
@@ -17,21 +29,18 @@ exports.sendEmail = function (req, res) {
             pass: c.mailu.p, // generated ethereal password
         },
     });
-    /* 
-        // send mail with defined transport object
-        let info =   transporter.sendMail({
-            from: '"Fred Foo 👻" <foo@example.com>', // sender address
-            to: "michelnovellino.programador@gmail.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
-        }).then(r => {
-            console.log
-        }).catch(e => {
-            res.send(e)
-            return
-        })
-     */
+    // send mail with defined transport object
+    transporter.sendMail({
+        from: '"Cs100 Biometria 👻" <foo@example.com>', // sender address
+        to: c.mailu.u, // list of receivers
+        subject: subject, // Subject line
+        html: html, // html body
+    }).then(r => {
+        console.log
+    }).catch(e => {
+        res.send(e)
+        return
+    })
     //console.log("Message sent: %s", info);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
